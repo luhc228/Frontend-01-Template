cc9f78c316b2fd34b8059c034c265df208f4b44c
luhc228 <luhengchang228@gmail.com>
2020-05-13 11:31:34 +0800

    add realm.js

--- week05/NOTE.md
+++ week05/NOTE.md
@@ -1,1 +1,10 @@
 # 每周总结可以写在这里
+
+JS执行粒度
+- JS Context => Realm (Global里所有的)
+- 宏任务
+- 微任务（Promise）
+- 函数调用（Execution Context）
+- 语句/声明
+- 表达式
+- 直接量/变量/this
--- week05/realm.js
+++ week05/realm.js
@@ -1,6 +1,8 @@
+// 去重
 var set = new Set();
 
-var queue = [
+// global上的所有属性
+var globalProperties = [
   "eval",
   "isFinite",
   "isNaN",
@@ -50,20 +52,46 @@ var queue = [
   "Reflect",
 ]
 
+let queue = [];
+
+for (let p of globalProperties) {
+  queue.push({
+    path: [p],
+    object: this[p]
+  })
+}
+
 let current
 
 while (queue.length) {
   current = queue.shift();
-  console.log(current);
-  set.add(current)
-  for (let p of Object.getOwnPropertyNames(current)) {
-    var property = Object.getOwnPropertyDescriptor(current, p)
+  if (set.has(current.current)) {
+    continue
+  }
+  console.log(current.path.join("."))
+  set.add(current.object)
 
-    if (property.hasOwnProperty("value") && property.value instanceof Object) {
-
+  for (let p of Object.getOwnPropertyNames(current.object)) {
+    var property = Object.getOwnPropertyDescriptor(current.object, p)
+    if (property.hasOwnProperty("value") && (
+      (property.value !== null && (typeof property.value === "object" || typeof property.value === "object"))
+    ) && property.value instanceof Object) {
+      queue.push({
+        object: property.value,
+        path: current.path.concat([p])
+      })
     }
-    if (current[p] instanceof Object) {
-      queue.push(current[p])
+    if (property.hasOwnProperty("set") && typeof property.set === 'function') {
+      queue.push({
+        object: property.set,
+        path: current.path.concat([p])
+      })
+    }
+    if (property.hasOwnProperty("get") && typeof property.get === 'function') {
+      queue.push({
+        object: property.get,
+        path: current.path.concat([p])
+      })
     }
   }
 }
